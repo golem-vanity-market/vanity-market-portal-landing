577f0c2809fcb1418761813ccea5d02ef75077f1
scx1332 <sieciech@gmail.com>
2025-10-28 13:00:10 +0100

    Delete unused files

--- README.md
+++
@@ -1,7 +0,0 @@
-
-
-# no monorepo
-
-No monorepo, we lost too much time trying to make it work, 
-there is a lot of issues with dependencies and linking, weird bugs in npm.
-Only yarn worked well, but we want to keep it working in all package managers.
--- run_clean.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-project_dir = os.path.dirname(os.path.realpath(__file__))
-shutil.rmtree(os.path.join(project_dir, 'shared', "node_modules"), ignore_errors=True)
-shutil.rmtree(os.path.join(project_dir, 'backend', "node_modules"), ignore_errors=True)
-shutil.rmtree(os.path.join(project_dir, 'frontend', "node_modules"), ignore_errors=True)
-shutil.rmtree(os.path.join(project_dir, 'frontend', "dist"), ignore_errors=True)
